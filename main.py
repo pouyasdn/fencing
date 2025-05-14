6569b64d067fbc8a19c51ab3ab66b90feab98ee9
pouyasdn <74226742+pouyasdn@users.noreply.github.com>
2025-05-14 22:51:49 +0330

    new.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
 KV = '''
@@ -8,12 +8,27 @@ ScreenManager:
 
 <FirstScreen>:
     name: "first"
-    Label:
-        text: "This is the first screen"
+    BoxLayout:
+        orientation: "vertical"
+        spacing: 10
+        padding: 20
+
+        Label:
+            text: "Hello from ScreenManager!"
+            font_size: 24
+
+        Button:
+            text: "Click Me"
+            size_hint_y: None
+            height: "48dp"
 '''
 
-class TestApp(App):
+class FirstScreen(Screen):
+    pass
+
+class MyApp(App):
     def build(self):
         return Builder.load_string(KV)
 
-TestApp().run()
+if __name__ == '__main__':
+    MyApp().run()
